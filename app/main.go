51f451aabf28d3dc4f289d0730dc82e75ee75874
Andrew Trubitsin <truba24032016@mail.ru>
2021-06-05 20:38:32 +0500

    Добавить несколько различных событий

--- app/main.go
+++ app/main.go
@@ -9,19 +9,27 @@ import (
 func main() {
 	var resolver = newResolver()
 
-	var event = eventSimple{id: guuid.New().String(), msg: "Hello"}
-	var event2 = eventSimple{id: guuid.New().String(), msg: "Bye"}
+	var event = CalcEventMinus{
+		eventSimple: eventSimple{id: guuid.New().String(), msg: "Minus"},
+		A:           40,
+		B:           20,
+	}
+	var event2 = CalcEventPlus{
+		eventSimple: eventSimple{id: guuid.New().String(), msg: "Plus"},
+		A:           5,
+		B:           5,
+	}
 
 	var subscriber1 = &subscriber{
 		id:     guuid.New().String(),
 		stop:   make(chan struct{}),
-		events: make(chan IEvent),
+		events: make(chan ICalcEvent),
 	}
 
 	var specialSubcriber = &specialSubscriber{subscriber{
 		id:     guuid.New().String(),
 		stop:   make(chan struct{}),
-		events: make(chan IEvent),
+		events: make(chan ICalcEvent),
 	}}
 
 	resolver.mySubcriber <- specialSubcriber
@@ -36,31 +44,14 @@ func main() {
 	time.Sleep(2000)
 }
 
-type IEvent interface {
-	Id() string
-	Msg() string
-}
-
-type eventSimple struct {
-	id  string
-	msg string
-}
-
-func (ev eventSimple) Id() string {
-	return ev.id
-}
-func (ev eventSimple) Msg() string {
-	return ev.msg
-}
-
 type resolver struct {
-	myEvent     chan IEvent
+	myEvent     chan ICalcEvent
 	mySubcriber chan ISubscriber
 }
 
 func newResolver() *resolver {
 	r := &resolver{
-		myEvent:     make(chan IEvent),
+		myEvent:     make(chan ICalcEvent),
 		mySubcriber: make(chan ISubscriber),
 	}
 
@@ -98,56 +89,4 @@ func (r *resolver) broadcastEvent() {
 			}
 		}
 	}
-}
-
-type ISubscriber interface {
-	GetId() string
-	HandleMessage()
-	GetStop() <-chan struct{}
-	GetEvents() chan IEvent
-}
-
-type subscriber struct {
-	id     string
-	stop   <-chan struct{}
-	events chan IEvent
-}
-
-type specialSubscriber struct {
-	subscriber
-}
-
-func (s subscriber) GetEvents() chan IEvent {
-	return s.events
-}
-
-func (s subscriber) GetStop() <-chan struct{} {
-	return s.stop
-}
-
-func (s subscriber) GetId() string {
-	return s.id
-}
-
-func (s subscriber) HandleMessage() {
-	go func() {
-		for {
-			select {
-			case ev := <-s.events:
-				log.Println("Simple subs", ev.Msg())
-			}
-		}
-	}()
-}
-
-/// overrides method
-func (s specialSubscriber) HandleMessage() {
-	go func() {
-		for {
-			select {
-			case ev := <-s.events:
-				log.Println("Special subs", ev.Msg())
-			}
-		}
-	}()
 }
